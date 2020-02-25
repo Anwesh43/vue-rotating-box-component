b5b3abe8180199b1026737c7cf1a2ab4adbc130d
Anwesh43 <anweshthecool0@gmail.com>
2020-02-25 22:08:04 +0530

    adding State class

---
+++ RotatingSquareComponent.js
@@ -0,0 +1,24 @@
+const scGap = 0.02
+class State {
+
+    scale = 0
+    dir = 0
+    prevScale = 0
+
+    update(cb) {
+        this.scale += scGap * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
